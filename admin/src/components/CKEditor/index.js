8a748ca9b269d233e09e0054fa373ed60631fdb3
Mickaël Tordjman <mickael.tdj@gmail.com>
2021-05-15 18:07:38 +0200

    custom build with math lib

--- admin/src/components/CKEditor/index.js
+++ admin/src/components/CKEditor/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from 'ckeditor5-build-strapi-wysiwyg';
+import ClassicEditor from 'custom_build-ckeditor5-build-strapi-wysiwyg';
 import styled from 'styled-components';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/ar';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/cs';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/de';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/es';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/fr';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/id';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/it';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/ko';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/nl';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/pl';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/pt';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/pt-br';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/ru';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/sk';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/th';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/tr';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/uk';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/vi';
-import 'ckeditor5-build-strapi-wysiwyg/build/translations/zh';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/ar';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/cs';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/de';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/es';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/fr';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/id';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/it';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/ko';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/nl';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/pl';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/pt';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/pt-br';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/ru';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/sk';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/th';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/tr';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/uk';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/vi';
+import 'custom_build-ckeditor5-build-strapi-wysiwyg/build/translations/zh';
 
 
 const Wrapper = styled.div`
